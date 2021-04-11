80ab7cc3fd4261e03f9e7ca7fec6439a7541da4c
Bellraj Eapen <github@gulfdoctor.net>
2021-04-11 15:16:19 -0700

    CommandLine demo application

--- application/src/main/java/com/canehealth/ckblib/application/DemoApplication.java
+++ application/src/main/java/com/canehealth/ckblib/application/DemoApplication.java
@@ -1,27 +1,63 @@
 package com.canehealth.ckblib.application;
 
 import com.canehealth.ckblib.service.MyService;
+import com.canehealth.ckblib.util.CkblibConstants;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.CommandLineRunner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication(scanBasePackages = "com.canehealth.ckblib")
-@RestController
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
 
 	private final MyService myService;
+	private static Logger LOG = LoggerFactory.getLogger(DemoApplication.class);
 
-	public DemoApplication(MyService myService) {
+	public DemoApplication (MyService myService) {
 		this.myService = myService;
-	}
-
-	@GetMapping("/")
-	public String home() {
-		return myService.message();
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) {
+		LOG.info("EXECUTING : " + myService.message());
+
+		String _function = "";
+		String _source = "";
+		String _destination = "";
+		try {
+			if (args[0].toLowerCase().equals("help")) {
+				System.out.println(CkblibConstants.HELPSTRING);
+			} else {
+				for (int i = 0; i < args.length; ++i) {
+					LOG.debug("args[{}]: {}", i, args[i]);
+					_function = args[0];
+					_source = args[1];
+					_destination = args[2];
+				}
+			}
+
+			if (_function.equals("tofhirbundle")) {
+				try {
+
+				} catch (Exception e) {
+					System.out.println(CkblibConstants.HELPSTRING);
+				}
+			} else if (_function.equals("tofhirserver")) {
+				System.out.println("To FHIR SERVER not implemented yet");
+			} else if (_function.equals("toomop")) {
+
+			} else if (_function.toLowerCase().equals("help")) {
+				System.out.println(CkblibConstants.HELPSTRING);
+			}
+		} catch (Exception e) {
+			System.out.println(CkblibConstants.HELPSTRING);
+		}
+	}
+
 }
