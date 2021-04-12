3dafd517f2a7434fce2456595f17b31090b1684d
Bell Eapen <github_public@gulfdoctor.net>
2021-04-12 18:22:34 -0400

    Add publish workflow

--- library/src/test/java/com/canehealth/ckblib/service/MyServiceTest.java
+++ library/src/test/java/com/canehealth/ckblib/service/MyServiceTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootTest("service.message=Hello")
 public class MyServiceTest {
@@ -20,6 +22,8 @@ public class MyServiceTest {
 	}
 
 	@SpringBootApplication
+	@Configuration
+	@ComponentScan(basePackages = "com.canehealth.ckblib")
 	static class TestConfiguration {
 	}
 
