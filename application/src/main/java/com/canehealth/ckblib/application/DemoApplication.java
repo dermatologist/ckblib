55d04436b0659be04f6f56075de2a28c713246db
Bell Eapen <github_public@gulfdoctor.net>
2021-04-15 16:31:21 -0400

    Added to application

--- application/src/main/java/com/canehealth/ckblib/application/DemoApplication.java
+++ application/src/main/java/com/canehealth/ckblib/application/DemoApplication.java
@@ -1,9 +1,17 @@
 package com.canehealth.ckblib.application;
 
 import com.canehealth.ckblib.library.service.MyService;
-import com.canehealth.ckblib.library.util.CkblibConstants;
+
+import java.util.concurrent.TimeUnit;
+
+import com.canehealth.ckblib.library.model.BaseQuery;
+import com.canehealth.ckblib.library.service.CkbEfetch;
+import com.canehealth.ckblib.qtakes.service.QtakesService;
+import com.canehealth.ckblib.qtakes.model.QtakesRoot;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 
 import org.slf4j.Logger;
@@ -14,6 +22,14 @@ public class DemoApplication implements CommandLineRunner {
 
 	private final MyService myService;
 	private static Logger LOG = LoggerFactory.getLogger(DemoApplication.class);
+	@Autowired
+	private BaseQuery baseQuery;
+
+	@Autowired
+	private CkbEfetch ckbEfetch;
+
+	@Autowired
+	private QtakesService qtakesService;
 
 	public DemoApplication (MyService myService) {
 		this.myService = myService;
@@ -24,40 +40,23 @@ public class DemoApplication implements CommandLineRunner {
 	}
 
 	@Override
-	public void run(String... args) {
+	public void run(String... args) throws InterruptedException {
 		LOG.info("EXECUTING : " + myService.message());
+		baseQuery.setTerm("Lichen Planus");
+		ckbEfetch.setBaseQuery(baseQuery);
+		TimeUnit.SECONDS.sleep(3);
+		ckbEfetch.get();
+		TimeUnit.SECONDS.sleep(5);
+		ckbEfetch.getPubmedArticleSet();
+		String myPublications = ckbEfetch.getAbstractsAsString(10);
+		System.out.print(myPublications);
 
-		String _function = "";
-		String _source = "";
-		String _destination = "";
-		try {
-			if (args[0].toLowerCase().equals("help")) {
-				System.out.println(CkblibConstants.HELP_STRING);
-			} else {
-				for (int i = 0; i < args.length; ++i) {
-					LOG.debug("args[{}]: {}", i, args[i]);
-					_function = args[0];
-					_source = args[1];
-					_destination = args[2];
-				}
-			}
+		qtakesService.setQtakesUrl("http://127.0.0.1:8093");
+		qtakesService.post(myPublications);
+		TimeUnit.SECONDS.sleep(3);
+		QtakesRoot r = qtakesService.getQtakesResults();
+		System.out.print(r.toString());
 
-			if (_function.equals("tofhirbundle")) {
-				try {
-
-				} catch (Exception e) {
-					System.out.println(CkblibConstants.HELP_STRING);
-				}
-			} else if (_function.equals("tofhirserver")) {
-				System.out.println("To FHIR SERVER not implemented yet");
-			} else if (_function.equals("toomop")) {
-
-			} else if (_function.toLowerCase().equals("help")) {
-				System.out.println(CkblibConstants.HELP_STRING);
-			}
-		} catch (Exception e) {
-			System.out.println(CkblibConstants.HELP_STRING);
-		}
 	}
 
 }
