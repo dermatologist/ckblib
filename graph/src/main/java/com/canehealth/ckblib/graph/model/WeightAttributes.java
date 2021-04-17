79f370d77fbe1bd71eef791e90fad015e1cc5889
Bell Eapen <github_public@gulfdoctor.net>
2021-04-17 17:53:37 -0400

    fix: loop in WeightAttributes

--- graph/src/main/java/com/canehealth/ckblib/graph/model/WeightAttributes.java
+++ graph/src/main/java/com/canehealth/ckblib/graph/model/WeightAttributes.java
@@ -1,7 +1,5 @@
 package com.canehealth.ckblib.graph.model;
 
-import java.util.List;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,10 +7,12 @@ import lombok.Data;
 @AllArgsConstructor
 public class WeightAttributes {
 
-    private List<JournalArticle> confidence;
+    // private List<JournalArticle> confidence;
 
-    private int upVote;
+    private Long confidence;
 
-    private int downVote;
+    private Long upVote;
+
+    private Long downVote;
 
 }
