c2c01d574e2257d867bc6f41f962b678234280ba
Bell Eapen <github_public@gulfdoctor.net>
2021-04-20 14:59:42 -0400

    Update d3 in all

--- graph/src/main/java/com/canehealth/ckblib/graph/AnatomicalSiteService.java
+++ graph/src/main/java/com/canehealth/ckblib/graph/AnatomicalSiteService.java
@@ -1,13 +1,8 @@
 package com.canehealth.ckblib.graph;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 import com.canehealth.ckblib.graph.model.AnatomicalSiteMention;
 
 import org.neo4j.driver.Driver;
-import org.neo4j.driver.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,6 +15,9 @@ public class AnatomicalSiteService {
 
     @Autowired
     AnatomicalSiteRepository anatomicalSiteMentionRepository;
+
+    @Autowired
+    D3MapBuilder d3MapBuilder;
 
     @Autowired
     Driver driver;
@@ -42,46 +40,10 @@ public class AnatomicalSiteService {
         return anatomicalSiteMentionRepository.mergeDiseaseWithAnatomy(dcui, scui, confidence, upvote, downvote);
     }
 
-    /**
-     * This is an example of when you might want to use the pure driver in case you
-     * have no need for mapping at all, neither in the form of the way the
-     * {@link org.springframework.data.neo4j.core.Neo4jClient} allows and not in
-     * form of entities.
-     *
-     * @return A representation D3.js can handle
-     */
-    public Map<String, List<Object>> forD3(String cui) {
+    public String forD3(String cui) {
 
-        var nodes = new ArrayList<>();
-        var links = new ArrayList<>();
+        return d3MapBuilder.build("Disease", "", "Anatomy", cui, "ANATOMY_OF");
 
-        try (Session session = driver.session()) {
-            var records = session
-                    .readTransaction(tx -> tx.run("" + " MATCH (d:Disease) <- [r:ANATOMY_OF] - (s:Anatomy {cui: '" + cui
-                            + "'})"
-                            + " WITH d, s, r ORDER BY r.confidence, d.name"
-                            + " RETURN d.name AS disease, collect(s.name) AS anatomys").list());
-            records.forEach(record -> {
-                var disease = Map.of("label", "disease", "title", record.get("disease").asString());
-
-                var targetIndex = nodes.size();
-                nodes.add(disease);
-
-                record.get("anatomys").asList(v -> v.asString()).forEach(name -> {
-                    var anatomy = Map.of("label", "anatomy", "title", name);
-
-                    int sourceIndex;
-                    if (nodes.contains(anatomy)) {
-                        sourceIndex = nodes.indexOf(anatomy);
-                    } else {
-                        nodes.add(anatomy);
-                        sourceIndex = nodes.size() - 1;
-                    }
-                    links.add(Map.of("source", sourceIndex, "target", targetIndex));
-                });
-            });
-        }
-        return Map.of("nodes", nodes, "links", links);
     }
 
 }
--- graph/src/main/java/com/canehealth/ckblib/graph/ProcedureService.java
+++ graph/src/main/java/com/canehealth/ckblib/graph/ProcedureService.java
@@ -1,13 +1,8 @@
 package com.canehealth.ckblib.graph;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 import com.canehealth.ckblib.graph.model.ProcedureMention;
 
 import org.neo4j.driver.Driver;
-import org.neo4j.driver.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,6 +15,9 @@ public class ProcedureService {
 
     @Autowired
     ProcedureRepository procedureMentionRepository;
+
+    @Autowired
+    D3MapBuilder d3MapBuilder;
 
     @Autowired
     Driver driver;
@@ -41,47 +39,11 @@ public class ProcedureService {
         return procedureMentionRepository.mergeDiseaseWithProcedure(dcui, scui, confidence, upvote, downvote);
     }
 
-    /**
-     * This is an example of when you might want to use the pure driver in case you
-     * have no need for mapping at all, neither in the form of the way the
-     * {@link org.springframework.data.neo4j.core.Neo4jClient} allows and not in
-     * form of entities.
-     *
-     * @return A representation D3.js can handle
-     */
-    public Map<String, List<Object>> forD3(String cui) {
 
-        var nodes = new ArrayList<>();
-        var links = new ArrayList<>();
+    public String forD3(String cui) {
 
-        try (Session session = driver.session()) {
-            var records = session
-                    .readTransaction(tx -> tx.run("" + " MATCH (d:Disease) <- [r:TREATMENT_OF] - (s:Procedure {cui: '"
-                            + cui
-                            + "'})"
-                            + " WITH d, s, r ORDER BY r.confidence, d.name"
-                            + " RETURN d.name AS disease, collect(s.name) AS procedures").list());
-            records.forEach(record -> {
-                var disease = Map.of("label", "disease", "title", record.get("disease").asString());
+        return d3MapBuilder.build("Disease", "", "Procedure", cui, "TREATMENT_OF");
 
-                var targetIndex = nodes.size();
-                nodes.add(disease);
-
-                record.get("procedures").asList(v -> v.asString()).forEach(name -> {
-                    var procedure = Map.of("label", "procedure", "title", name);
-
-                    int sourceIndex;
-                    if (nodes.contains(procedure)) {
-                        sourceIndex = nodes.indexOf(procedure);
-                    } else {
-                        nodes.add(procedure);
-                        sourceIndex = nodes.size() - 1;
-                    }
-                    links.add(Map.of("source", sourceIndex, "target", targetIndex));
-                });
-            });
-        }
-        return Map.of("nodes", nodes, "links", links);
     }
 
 }
