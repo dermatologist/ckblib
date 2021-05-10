6a35f2778976f8f3aa64e93aece31347182056ed
Bell Eapen <github_public@gulfdoctor.net>
2021-05-10 06:44:36 -0400

    Add disease delete

--- graph/src/main/java/com/canehealth/ckblib/graph/DiseaseDisorderService.java
+++ graph/src/main/java/com/canehealth/ckblib/graph/DiseaseDisorderService.java
@@ -7,20 +7,50 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
 public class DiseaseDisorderService {
 
-
     @Autowired
     DiseaseDisorderRepository diseaseDisorderMentionRepository;
-
-
 
     @Autowired
     Driver driver;
 
+    /**
+     * Returns all diseases in the graph
+     *
+     * @see com.canehealth.ckblib.graph.DiseaseDisorderRepository
+     * @author beapen
+     * @since 0.14.0
+     * @return All disease nodes in the graph
+     */
+    public Flux<DiseaseDisorderMention> getAllDiseases(){
+        return diseaseDisorderMentionRepository.findAll();
+    }
+
+    /**
+     * Finds a disease by CUI to which the
+     * {@link #deleteDisease(DiseaseDisorderMention diseaseDisorderMention)) subscribes}
+     *
+     * @since 0.14.0
+     * @param cui CUI
+     */
+    public void deleteDiseaseByCui(String cui) {
+        diseaseDisorderMentionRepository.findOneByCui(cui).subscribe(this::deleteDisease);
+    }
+
+    /**
+     * Deletes a disease node
+     * @since 0.14.0
+     * @param diseaseDisorderMention Disease NodeEntity to delete
+     */
+    @Transactional
+    public void deleteDisease(DiseaseDisorderMention diseaseDisorderMention){
+        diseaseDisorderMentionRepository.delete(diseaseDisorderMention);
+    }
 
     public Mono<DiseaseDisorderMention> getDiseaseByCui(String cui) {
         return diseaseDisorderMentionRepository.findOneByCui(cui);
