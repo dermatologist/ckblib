1607e8f02a77dcff339329946e568b8f9b3cd85a
Bell Eapen <github_public@gulfdoctor.net>
2021-06-04 20:26:15 +0000

    tests: cui test

--- umls/src/test/java/com/canehealth/ckblib/umls/service/RestTicketServiceTest.java
+++ umls/src/test/java/com/canehealth/ckblib/umls/service/RestTicketServiceTest.java
@@ -3,6 +3,7 @@ package com.canehealth.ckblib.umls.service;
 import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 import com.canehealth.ckblib.umls.model.SearchRoot;
+import com.canehealth.ckblib.umls.model.SingletonRoot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,12 +15,16 @@ import reactor.core.publisher.Mono;
 class RestTicketServiceTest {
 
     @Test
-    void shouldRetrieveTgt() throws InterruptedException{
+    void shouldSearchAndReturnResults() throws InterruptedException{
 
         Exception exception = assertThrows(
                 org.springframework.web.reactive.function.client.WebClientResponseException.class, () -> {
 
-                    // * USAGE EXAMPLE with QueryParams
+                    /*
+                        USAGE EXAMPLE
+                        Constructor gets tgt refreshing token every 6 hours.
+                        So reuse the same object.
+                    */
                     RestTicketService restTicketService = new RestTicketService("YOURUMLSAPIKEY");
                     Mono<SearchRoot> result = restTicketService.webClient().get()
                             .uri(uriBuilder -> uriBuilder.path("/search/current")
@@ -39,6 +44,32 @@ class RestTicketServiceTest {
 
     }
 
+    @Test
+    void shouldRetrieveCui() throws InterruptedException {
+
+        Exception exception = assertThrows(
+                org.springframework.web.reactive.function.client.WebClientResponseException.class,
+                () -> {
+
+                    // * USAGE EXAMPLE with QueryParams
+                    RestTicketService restTicketService = new RestTicketService("YOURUMLSAPIKEY");
+                    Mono<SingletonRoot> result = restTicketService.webClient().get()
+                            .uri(uriBuilder -> uriBuilder.path("/content/current/CUI/{cui}/atoms")
+                                    .queryParam("ticket", restTicketService.getSt())
+                                    .queryParam("searchType", "exact").build("C0155502"))
+                            .retrieve().bodyToMono(SingletonRoot.class);
+                    /*
+                     * <-- WITH URL PARAMS --> this.webClient.get() .uri(uriBuilder - > uriBuilder
+                     * .path("/products/{id}/attributes/{attributeId}") .build(2, 13)) .retrieve();
+                     */
+                    System.out.println(result.block());
+                });
+        String expectedMessage =
+                "401 Unauthorized from POST https://utslogin.nlm.nih.gov/cas/v1/tickets";
+        String actualMessage = exception.getMessage();
+        assertTrue(actualMessage.contains(expectedMessage));
+
+    }
 
     @SpringBootApplication
     @Configuration
